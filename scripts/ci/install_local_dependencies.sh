3f6c9549d47361056c8385378c7edfda9fdb7428
Aaron Weiker <aaron@weiker.org>
2016-02-04 19:08:29 -0800

    Changing the order of brunch and coffeescript

--- scripts/ci/install_local_dependencies.sh
+++ scripts/ci/install_local_dependencies.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 mix local.hex --force && mix local.rebar --force
-npm install -g brunch
 npm install -g coffee-script
+npm install -g brunch
 mix do deps.get, deps.compile
 npm install
